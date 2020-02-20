96c0afea1e90a28307f62b7873bbb3cef12dbba0
ysiin <zachbone0@gmail.com>
2020-02-20 19:59:11 +0700

    menemukan angka terbesar dari array

--- find_the_largest_num.js
+++ find_the_largest_num.js
@@ -1,8 +1,10 @@
 // buat sebuah function yang me-return angka paling besar dari sebuah array of numbers;
 
-
+function findLargestNum(nums) {
+    return Math.max.apply(null, nums)
+}
 
 // there are no negative numbers.
-findLargestNum([4, 5, 1, 3]) // 5
-findLargestNum([300, 200, 600, 150]) // 600
-findLargestNum([1000, 1001, 857, 1]) // 1001
+console.log(findLargestNum([4, 5, 1, 3])) // 5
+console.log(findLargestNum([300, 200, 600, 150])) // 600
+console.log(findLargestNum([1000, 1001, 857, 1])) // 1001 
