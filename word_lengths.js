6e5dc6bfab4827070618a07e9248dc02419aed18
ysiin <zachbone0@gmail.com>
2020-02-20 20:35:22 +0700

    word lengths sementara

--- word_lengths.js
+++ word_lengths.js
@@ -2,12 +2,16 @@
 //dan mengubahnya menjadi array of panjang kata tersebut 
 
 function wordLengths(arr) {
-	
+    for (let i = 0; i < arr.length; i++) {
+        if (i < arr[i].length) {
+            return arr[i].length
+        }
+    }
 }
 
 // TIPS:
 /* gunakan array methods seperti array.push */
 
-wordLengths(["hello", "world"]) // [5, 5]
-wordLengths(["Halloween", "Thanksgiving", "Christmas"]) // [9, 12, 9]
-wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]) // [3, 5, 9, 4, 2, 3, 8]
+console.log(wordLengths(["hello", "world"])) // [5, 5]
+console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"])) // [9, 12, 9]
+console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"])) // [3, 5, 9, 4, 2, 3, 8]
